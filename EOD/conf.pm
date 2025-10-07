f0a88f40a59216d9585472511ce21576b9fd0d52
Ian LeCorbeau <I-LeCorbeau@protonmail.com>
2025-10-07 10:07:14 -0400

    add conf.pm

---
+++ EOD/conf.pm
@@ -0,0 +1,26 @@
+# $Ragnarok$
+# Module to parse config.
+
+package EOD::conf;
+
+use strict;
+use warnings;
+use File::HomeDir;
+use Config::General;
+our @EXPORT_OK = qw($username $sign $verify_sig $pubkey_dir);
+
+my $conffile	= File::HomeDir->my_home . '/.rcs.conf';
+
+my $conf = Config::General->new(
+	-ConfigFile		=> $conffile,
+	-SplitPolicy		=> 'custom',
+	-SplitDelimiter		=> '\s+=\s+',
+	-InterPolateVars	=> 1,
+	-AutoTrue		=> 1
+);
+
+my %config	= $conf->getall;
+our $username	= $config{'USERNAME'};
+our $sign	= $config{'SIGN'};
+our $verify_sig	= $config{'VERIFY_SIG'};
+our $pubkey_dir	= $config{'PUBKEY_DIR'};
